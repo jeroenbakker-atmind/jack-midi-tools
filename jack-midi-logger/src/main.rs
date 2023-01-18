14450136e0e35f4440446e6b4598126167b4f2f3
Jeroen Bakker <jeroen@blender.org>
2023-01-18 12:38:42 +0100

    Hide runners from public interface.

--- jack-midi-logger/src/main.rs
+++ jack-midi-logger/src/main.rs
@@ -2,13 +2,11 @@
 //! out all values sent to it through the input port. It also sends a
 //! Note On and Off event, once every cycle, on the output port.
 
-use jack_module::{JackRunner, Runner};
+use jack_module::run_jack_module;
 use midi_logger::MidiLogger;
 
 mod midi_logger;
 
 pub fn main() {
-    let module = MidiLogger::default();
-    let runner = JackRunner::new(&module);
-    runner.run(module);
+    run_jack_module::<MidiLogger>();
 }
--- jack-module/src/lib.rs
+++ jack-module/src/lib.rs
@@ -4,4 +4,20 @@ mod runner;
 
 pub use module::*;
 pub use port_descriptor::*;
-pub use runner::*;
+use runner::{JackRunner, Runner};
+
+pub fn run_jack_module<T>()
+where
+    T: Module + Send + 'static + Default,
+{
+    let module = T::default();
+    run_as_jack_module(module);
+}
+
+pub fn run_as_jack_module<T>(module: T)
+where
+    T: Module + Send + 'static,
+{
+    let runtime = JackRunner::new(&module);
+    runtime.run(module);
+}
