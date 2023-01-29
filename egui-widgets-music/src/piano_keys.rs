7711b82f2cd8d3e02e63e3e5cb59684f990a3dfc
Jeroen Bakker <j.bakker@atmind.nl>
2023-01-29 19:31:53 +0100

    Drawing of black keys.

--- egui-widgets-music/src/piano_keys.rs
+++ egui-widgets-music/src/piano_keys.rs
@@ -1,14 +1,16 @@
-use egui::{Color32, Rect, Rounding, Widget};
+use egui::{Color32, Pos2, Rect, Rounding, Widget};
 use music_notes::{ChromaticNote, ChromaticTone};
 
 pub struct PianoConfig {
     pub first_key: ChromaticNote,
     pub last_key: ChromaticNote,
     pub white_key_size_ratio: f32,
+    pub black_key_size_ratio: f32,
     pub ratio_rounding: f32,
     pub color_white_key: Color32,
     pub color_black_key: Color32,
-    pub color_pressed_key: Color32,
+    pub color_white_pressed_key: Color32,
+    pub color_black_pressed_key: Color32,
 }
 
 impl Default for PianoConfig {
@@ -17,10 +19,12 @@ impl Default for PianoConfig {
             first_key: ChromaticNote::new(ChromaticTone::A, 0),
             last_key: ChromaticNote::new(ChromaticTone::C, 8),
             white_key_size_ratio: 6.0,
+            black_key_size_ratio: 3.0,
             ratio_rounding: 0.1,
             color_black_key: Color32::BLACK,
             color_white_key: Color32::WHITE,
-            color_pressed_key: Color32::GRAY,
+            color_white_pressed_key: Color32::GRAY,
+            color_black_pressed_key: Color32::DARK_GRAY,
         }
     }
 }
@@ -44,6 +48,7 @@ trait PianoKey {
     fn is_black_key(&self) -> bool {
         !self.is_white_key()
     }
+    fn is_followed_by_black_key(&self) -> bool;
 }
 
 impl PianoKey for ChromaticNote {
@@ -63,6 +68,22 @@ impl PianoKey for ChromaticNote {
             | ChromaticTone::GSharp => false,
         }
     }
+    fn is_followed_by_black_key(&self) -> bool {
+        match self.tone {
+            ChromaticTone::A
+            | ChromaticTone::C
+            | ChromaticTone::D
+            | ChromaticTone::F
+            | ChromaticTone::G => true,
+            ChromaticTone::E
+            | ChromaticTone::B
+            | ChromaticTone::ASharp
+            | ChromaticTone::CSharp
+            | ChromaticTone::DSharp
+            | ChromaticTone::FSharp
+            | ChromaticTone::GSharp => false,
+        }
+    }
 }
 
 impl Widget for PianoKeys {
@@ -76,6 +97,7 @@ impl Widget for PianoKeys {
         let white_key_width = canvas_width / number_of_white_keys as f32;
         let rounding = white_key_width * self.config.ratio_rounding;
         let white_key_height = white_key_width * self.config.white_key_size_ratio;
+        let black_key_height = white_key_width * self.config.black_key_size_ratio;
         let painter = ui.painter();
 
         // Draw white keys.
@@ -85,18 +107,18 @@ impl Widget for PianoKeys {
             .enumerate()
             .for_each(|(key_number, note)| {
                 let fill_color = if self.pressed_keys.contains(&note) {
-                    self.config.color_pressed_key
+                    self.config.color_white_pressed_key
                 } else {
                     self.config.color_white_key
                 };
                 let key = key_number as f32;
                 painter.rect_filled(
                     Rect {
-                        min: egui::Pos2 {
+                        min: Pos2 {
                             x: key * white_key_width + 1.0,
                             y: 50.0,
                         },
-                        max: egui::Pos2 {
+                        max: Pos2 {
                             x: (key + 1.0) * white_key_width - 1.0,
                             y: 50.0 + white_key_height,
                         },
@@ -112,6 +134,39 @@ impl Widget for PianoKeys {
             });
 
         // TODO: paint black keys.
+        (i32::from(self.config.first_key)..i32::from(self.config.last_key))
+            .map(|i| ChromaticNote::from(i))
+            .filter(|n| n.is_white_key())
+            .enumerate()
+            .filter(|(_i, note)| note.is_followed_by_black_key())
+            .map(|(i, note)| (i, note + 1))
+            .for_each(|(key_number, note)| {
+                let fill_color = if self.pressed_keys.contains(&note) {
+                    self.config.color_black_pressed_key
+                } else {
+                    self.config.color_black_key
+                };
+                let key = key_number as f32 + 0.5;
+                painter.rect_filled(
+                    Rect {
+                        min: Pos2 {
+                            x: key * white_key_width + 1.0,
+                            y: 50.0,
+                        },
+                        max: Pos2 {
+                            x: (key + 1.0) * white_key_width - 1.0,
+                            y: 50.0 + black_key_height,
+                        },
+                    },
+                    Rounding {
+                        ne: 0.0,
+                        nw: 0.0,
+                        se: rounding,
+                        sw: rounding,
+                    },
+                    fill_color,
+                );
+            });
 
         // Dummy label as we need to return a response.
         // TODO: we should construct a valid response.
